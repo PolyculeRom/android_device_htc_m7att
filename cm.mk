f8416c6ec7e25579bd7c79db94d155bdb5345ca5
Jason Southward <jasonsouthward@gmail.com>
2013-07-12 08:16:10 -0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -8,5 +8,5 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="cingular_us/m7/m7:4.1.2/JZO54K/166754.12:user/release-keys" PRIVATE_BUILD_DESC="1.26.502.12 CL166754 release-keys"
 
-PRODUCT_NAME := cm_m7att
+PRODUCT_NAME := poly_m7att
 PRODUCT_DEVICE := m7att
